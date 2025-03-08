7a0b63da5b76dcd3f814900647f3f206cd0926a1
AdilBayraktar <adilbayraktar1997@gmail.com>
2025-03-08 16:51:24 +0300

    Error Handling on Pord & Dev modes

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,24 @@
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
+};
+const sendErrorDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
     err: err,
     message: err.message,
     stack: err.stack,
   });
-};
+
+const sendErrorProd = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 
 module.exports = globalErrorHandler;
